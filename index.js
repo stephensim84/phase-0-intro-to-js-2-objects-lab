351bf651a1e49d4b432a0aeb4e28389fe39ddaed
Stephen Simmons <stephensimmons@stephens-MacBook-Pro.local>
2022-03-15 08:54:04 -0700

    commit tree 892deb5fe241b92c9a5b26e99c9301fbdae990a3

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+let employee = {
+    name: "Isaac",
+    address: "1427 26th ave"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key , value){
+    // employees.name = employee;
+    // let newarray = {...employees, key : value};
+    
+    return {...employee, [key]: value};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, text ){
+employee[streetAddress] = text;
+return employee
+}
+
+
+function deleteFromEmployeeByKey(employee, key){
+    let clone = employee;
+
+    delete clone [key];
+    return clone 
+} 
+
+function deleteFromEmployeeByKey2(employee, key){
+delete employee.name == key
+return employee
+}
